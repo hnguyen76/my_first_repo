39ca5a942e7c1cf738e61bd6fd5c32b3638aea0a
Hieu Nguyen <hieumercer@gmail.com>
2026-04-09 10:48:59 -0400

    f string added

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-print("Hello, World!")
-print("This is a test file.")
-print("Testing complete.")
+print("Hello, World!\n")
+print("This is a test file.\n")
+
+person = {
+    "name": "Hieu",
+    "age": 25,
+    "country": "USA"
+}
+print(f"My name is {person['name']}, my age is {person['age']} and I live in {person['country']}.")
+
+print("\nTesting complete.")
