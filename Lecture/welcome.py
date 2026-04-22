e545f43dacb39346849a5329785967e72c23a767
Hieu Nguyen <hieumercer@gmail.com>
2026-04-22 15:17:39 -0400

    Final commit

--- Lecture/welcome.py
+++ Lecture/welcome.py
@@ -1,31 +1,53 @@
+# This is a simple Python script that demonstrates basic syntax, comments, and input/output operations.
+import sys
+import math
+import os
+
+# from math import pi  # Importing only the pi constant from the math module.
+# print('Python version:', sys.version)  # Print the version of Python being used.
+# print(math.log10(100))  # Print the logarithm base 10 of 100.
+# print(math.sqrt(16))  # Print the square root of 16.
+# print(math.pi)  # Print the value of pi.
+# print(os.getcwd())  # Print the current working directory.
+# print(os.listdir())  # Print the list of files and directories in the current working directory.    
+# print(os.environ.get('number_of_processors'))  # Print the number of processors available in the system.
+# print('total: ', os.cpu_count())  # Print the number of CPUs in the system.
+# print(sys.path)  # Print the list of directories that Python searches for modules.
+
+# for path in sys.path:
+#     print(path)  # Print each directory in the sys.path list.
+
+print(sys.modules)  # Print the dictionary of loaded modules in the current Python session.
+
+
 # Single line comment using hash symbol.
 '''Multi line comment 
 using triple 
 quotes.  '''
 # To toggle comment 'ctrl + ?.  '
 
-print("Hello World")
-print(1+2, 3*4, 5/2, 5//2, 5%2, 2**3) # 5/2 is float division, 5//2 is floor division, 5%2 is modulus operator, 2**3 is exponentiation operator.
-print("Hello" + " " + "World")  # String concatenation.
-print("Hello " * 3)  # String repetition.
-print("Hello", "World")  # String concatenation with space in between.
-print(300.5+.5)  # Float addition.
-print(12,24,-2,sep=':')  # sep is used to specify the separator between the values. 
-print("Per Scholas is Unlocking Potential and Changing the Face of Tech")
-print(10+90)
-print(300.5+0.5)
-print(12, 24, -2, sep=':')
-print('but', 'not', 'including', sep='**')
-print('but', 'not', 'including', sep=' ')
+# print("Hello World")
+# print(1+2, 3*4, 5/2, 5//2, 5%2, 2**3) # 5/2 is float division, 5//2 is floor division, 5%2 is modulus operator, 2**3 is exponentiation operator.
+# print("Hello" + " " + "World")  # String concatenation.
+# print("Hello " * 3)  # String repetition.
+# print("Hello", "World")  # String concatenation with space in between.
+# print(300.5+.5)  # Float addition.
+# print(12,24,-2,sep=':')  # sep is used to specify the separator between the values. 
+# print("Per Scholas is Unlocking Potential and Changing the Face of Tech")
+# print(10+90)
+# print(300.5+0.5)
+# print(12, 24, -2, sep=':')
+# print('but', 'not', 'including', sep='**')
+# print('but', 'not', 'including', sep=' ')
 
-name=input('What is your name? ')
-print('Hello, ' + name + '! Welcome to Python programming.')
+# name=input('What is your name? ')
+# print('Hello, ' + name + '! Welcome to Python programming.')
 
-mb = input('What is your favorite movie? ')
-print('Great choice! ' + mb + ' is a fantastic movie.')
+# mb = input('What is your favorite movie? ')
+# print('Great choice! ' + mb + ' is a fantastic movie.')
 
-hp = input('What is your favorite hobby? ')
-print('That sounds fun! ' + hp + ' is a great way to spend your time.')
+# hp = input('What is your favorite hobby? ')
+# print('That sounds fun! ' + hp + ' is a great way to spend your time.')
 
-mobile_number = input('Please enter your mobile number: ')
-print('Mobile number: ' + mobile_number)
+# mobile_number = input('Please enter your mobile number: ')
+# print('Mobile number: ' + mobile_number)
