70dc0ae5b89a98b6646711b4f150c92e344a393e
Hieu Nguyen <hieumercer@gmail.com>
2026-04-22 14:20:43 -0400

    lecture folder created
    readme.md
    welcome.py
    two filed created

---
+++ Lecture/welcome.py
@@ -0,0 +1,32 @@
+# Single line comment using hash symbol.
+'''Multi line comment 
+using triple 
+quotes.  '''
+# To toggle comment 'ctrl + ?.  '
+
+# print("Hello World")
+# print(1+2, 3*4, 5/2, 5//2, 5%2, 2**3)
+# # 5/2 is float division, 5//2 is floor division, 5%2 is modulus operator, 2**3 is exponentiation operator.
+# print("Hello" + " " + "World")  # String concatenation.
+# print("Hello " * 3)  # String repetition.
+# print("Hello", "World")  # String concatenation with space in between.
+# print(300.5+.5)  # Float addition.
+# print(12,24,-2,sep=':')  # sep is used to specify the separator between the values. 
+# print("Per Scholas is Unlocking Potential and Changing the Face of Tech")
+# print(10+90)
+# print(300.5+0.5)
+# print(12, 24, -2, sep=':')
+# print('but', 'not', 'including', sep='**')
+# print('but', 'not', 'including', sep=' ')
+
+name=input('What is your name? ')
+print('Hello, ' + name + '! Welcome to Python programming.')
+
+mb = input('What is your favorite movie? ')
+print('Great choice! ' + mb + ' is a fantastic movie.')
+
+hp = input('What is your favorite hobby? ')
+print('That sounds fun! ' + hp + ' is a great way to spend your time.')
+
+mobile_number = input('Please enter your mobile number: ')
+print('Mobile number: ' + mobile_number)
